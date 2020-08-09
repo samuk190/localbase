2c473a64b75dbd3b536b7da2225cc77b69cd4d6f
Danny Connell <sendstufftodanny@gmail.com>
2020-08-09 17:19:27 +0100

    Come up with a list of user errors to handle

--- localbase/localbase.js
+++ localbase/localbase.js
@@ -1,35 +1,36 @@
 "use strict"
 
-// TODO: come up with a list of user errors to handle
+
 /*  
-  add()
+  collection()
     - no collection specified
-    - wrong type in collection()
+    - wrong/invalid type in collection()
+  doc()
+    - no doc specified
+    - wrong type in doc()
+  orderBy()
+    - no field specified
+    - first parameter not a string
+    - second parameter not a string == 'asc' or 'desc'
+  limit()
+    - no limit specified
+    - not an integer >= 1
+  add()
     - wrong type in add()
     - nothing specified in add()
   update()
-    - no collection specified
-    - wrong type in collection()
-    - no doc specified
-    - wrong type in doc()
     - wrong type in update()
     - nothing specified in update()
+    - document doesn't exist
   set()
-    - no collection specified
-    - wrong type in collection()
-    - no doc specified
-    - wrong type in doc()
     - wrong type in set()
     - nothing specified in set()
   get()
-    - no collection specified
-    - wrong type in collection()
     - wrong type in get()
     - invalid criteria / type in doc()
+    - document doesn't exist
   delete() 
-    - no collection / invalid type in collection()
     - collection doesn't exist
-    - invalid document criteria / type
     - document doesn't exist
   general
     - methods in wrong order
