cbdc4eb350d567ff9f7c3cda6dc388f301fdb3ed
Danny Connell <sendstufftodanny@gmail.com>
2020-09-04 16:14:16 +0100

    Added orderBy() error handling

--- localbase/api/filters/orderBy.js
+++ localbase/api/filters/orderBy.js
@@ -1,5 +1,20 @@
 export default function orderBy(property, direction) {
-  if (property) this.orderByProperty = property
-  if (direction) this.orderByDirection = direction
+  if (!property) {
+    this.selectorAndFilterErrors.push("No field name specified in orderBy() method. Use a string e.g. 'name'")
+  }
+  else if (typeof property !== 'string') {
+    this.selectorAndFilterErrors.push("First parameter in orderBy() method must be a string (a field name) e.g. 'name'")
+  }
+  else {
+    this.orderByProperty = property
+  }
+  if (direction) {
+    if (direction !== 'asc' && direction !== 'desc') {
+      this.selectorAndFilterErrors.push("Second parameter in orderBy() method must be a string set to 'asc' or 'desc'.")
+    }
+    else {
+      this.orderByDirection = direction
+    }
+  }
   return this
 }
--- localbase/localbase.js
+++ localbase/localbase.js
@@ -10,10 +10,10 @@
   doc() ✅
     - no doc specified ✅
     - wrong type in doc() ✅
-  orderBy()
-    - no field specified
-    - first parameter not a string
-    - second parameter not a string == 'asc' or 'desc'
+  orderBy() ✅
+    - no field specified ✅
+    - first parameter not a string ✅
+    - second parameter not a string == 'asc' or 'desc' ✅
   limit()
     - no limit specified
     - not an integer >= 1
