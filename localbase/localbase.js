e63590b6de48de582626321a80f4113b098349db
Danny Connell <sendstufftodanny@gmail.com>
2020-09-04 14:38:46 +0100

    Removed Localbase Editor (Vue) and put in it's own private repo

--- localbase/localbase.js
+++ localbase/localbase.js
@@ -1,6 +1,5 @@
 "use strict"
 
-// TODO: put localbase-playground & localbase-editor-vue in their own repos
 // TODO: create an errors array on the class - push errors into this in the various methods
 //       and then in the actions, check for errors and reject, logging them all out
 //       REMEMBER to clear the errors array in the reset method
