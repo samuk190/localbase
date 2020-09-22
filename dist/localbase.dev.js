f312a9b932c88b54c8f0517aa49781b2005e8e3c
Danny Connell <sendstufftodanny@gmail.com>
2020-09-22 10:08:31 +0100

    Bug fix: Update doesn't work when passing an array as a field value

--- dist/localbase.dev.js
+++ dist/localbase.dev.js
@@ -948,16 +948,17 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = updateObject;
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function updateObject(obj
 /*, …*/
 ) {
   for (var i = 1; i < arguments.length; i++) {
     for (var prop in arguments[i]) {
-      var val = arguments[i][prop];
-      if (_typeof(val) == "object") // this also applies to arrays or null!
-        update(obj[prop], val);else obj[prop] = val;
+      var val = arguments[i][prop]; // if (typeof val == "object") // this also applies to arrays or null!
+      //   updateObject(obj[prop], val);
+      // else
+      //    obj[prop] = val;
+
+      obj[prop] = val;
     }
   }
 
--- localbase/utils/updateObject.js
+++ localbase/utils/updateObject.js
@@ -2,10 +2,11 @@ export default function updateObject(obj/*, …*/) {
   for (var i=1; i<arguments.length; i++) {
       for (var prop in arguments[i]) {
           var val = arguments[i][prop];
-          if (typeof val == "object") // this also applies to arrays or null!
-              update(obj[prop], val);
-          else
-              obj[prop] = val;
+          // if (typeof val == "object") // this also applies to arrays or null!
+          //   updateObject(obj[prop], val);
+          // else
+          //    obj[prop] = val;
+          obj[prop] = val;
       }
   }
   return obj;
